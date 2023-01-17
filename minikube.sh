4b6a3e3b22b73cf446188d66281c8a83251845a3
nandu2311 <nandu.racharla1991@gmail.com>
2023-01-17 21:17:57 +0530

    update minikube

--- minikube.sh
+++ minikube.sh
@@ -3,6 +3,15 @@
 sudo su
 sudo apt-get update -y
 sudo apt-get install docker.io -y
+
+#download kubectl and install it
+
+curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl && chmod +x ./kubectl && sudo mv ./kubectl /usr/local/bin/kubectl
+
+#downloading minikube and configuring
+
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 && chmod -x minikube && sudo mv minikube /usr/local/bin
+
 sudo apt-get install conntrack
 sudo minikube start --vm-driver=none
 minikube status
