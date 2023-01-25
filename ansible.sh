8a52b1bda92bda49789d99f6691690a169521152
nandu2311 <nandu.racharla1991@gmail.com>
2023-01-25 21:39:58 +0530

    update minikube and ansible scripts

--- ansible.sh
+++ ansible.sh
@@ -2,3 +2,6 @@ sudo apt update -y
 sudo apt install software-properties-common -y
 sudo apt-add-repository --yes --update ppa:ansible/ansible
 sudo apt install ansible -y
+sudo apt install python3-pip 
+sudo pip install boto3
+ansible-galaxy collection install amazon.aws
